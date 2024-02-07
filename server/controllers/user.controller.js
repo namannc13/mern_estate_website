c37d10dd39077a8d5245a98db3f7e51313d692ae
chomchom13 <chawhannaman5@gmail.com>
2024-02-07 21:50:04 +0530

    added contact landlord functionality with mail, added click to copy the url of the site to clipboard, added getUser server route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -56,4 +56,16 @@ const getUserListings = async(req, res, next) => {
   }
 }
 
-export { updateUser, deleteUser, getUserListings };
+const getUser = async(req,res,next) => {
+  try {
+    const user = await userModel.findById(req.params.id)
+    if(!user) return next(errorHandler(404, 'User not found'))
+    const { password: pass, ...rest} = user._doc;
+    res.status(200).json(rest)
+    
+  } catch (error) {
+    next(error)
+  }
+}
+
+export { updateUser, deleteUser, getUserListings, getUser };
