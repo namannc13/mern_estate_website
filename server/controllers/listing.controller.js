c9bc80a7ed5325a185135e575b38913ffab86580
chomchom13 <chawhannaman5@gmail.com>
2024-02-07 17:28:34 +0530

    created updateListing route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -12,17 +12,38 @@ const createListing = async (req, res, next) => {
 
 const deleteListing = async (req, res, next) => {
   const listing = await listingModel.findById(req.params.id);
-  if (!listing) return next(errorHandler(404, "listing not found!"));
-
+  if (!listing) {
+    return next(errorHandler(404, "listing not found!"));
+  }
   if (req.user.id !== listing.userRef) {
     return next(errorHandler(401, "You can only delete your own listings!"));
   }
   try {
-    await listingModel.findByIdAndDelete(req.params.id); 
-    res.status(200).json("Listing has been deleted!")
+    await listingModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
   } catch (error) {
     next(error);
   }
 };
 
-export { createListing, deleteListing };
+const updateListing = async (req, res, next) => {
+  const listing = await listingModel.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+  try {
+    const updatedListing = await listingModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createListing, deleteListing, updateListing };
--- server/utils/error.js
+++ server/utils/error.js
@@ -1,3 +1,5 @@
+
+
 const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
